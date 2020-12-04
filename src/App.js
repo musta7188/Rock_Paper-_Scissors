5e97e59858b6afbaefb90f6620abf4ccd1a5f3e5
musta7188 <musta7188@hotmail.it>
2020-12-04 22:36:54 +0000

    set up app for React application

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+
+export function App({ user }) {
+
+  return(
+    <div>
+      App live
+    </div>
+  )
+}
+
+
+export default App
---
+++ src/App.test.js
@@ -0,0 +1,20 @@
+// import {App} from './App';
+// import { render } from './test-utils'
+
+// describe('App', () => { 
+//   it('renders correctly when user is not provided', () => { 
+//     const { container } = render(<App />);
+//     expect(container.firstChild).toMatchSnapshot();
+//     expect(container.firstChild).toHaveClass('App');
+//   });
+
+//   it('renders correctly when user is provided', () => {
+//     const user = {
+//       name: '',
+//     }
+//     const { container } = render(<App user={user} />);
+//     expect(container.firstChild).toMatchSnapshot();
+//     expect(container.firstChild).toHaveClass('Game');
+
+//   })
+// })
